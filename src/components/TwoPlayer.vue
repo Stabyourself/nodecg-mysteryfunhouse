0dea01256ea0aa380873cecede493b26e50280e8
Maurice <maurice@guegan.de>
2021-09-17 17:14:22 +0200

    parent

--- src/components/TwoPlayer.vue
+++ src/components/TwoPlayer.vue
@@ -94,7 +94,7 @@ let twitchOptions = {
     channel: null,
     autoplay: true,
     muted: false,
-    parent: ["nodecg.guegan.de:9090"]
+    parent: ["nodecg.guegan.de"]
 }
 let player1, player2
 
