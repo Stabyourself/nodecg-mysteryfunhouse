63d68ef67be32690398b38340fae147a2afd7560
Maurice <maurice@guegan.de>
2021-09-29 20:48:58 +0200

    More woff2 = more cool

--- src/js/components/PlayerCard.vue
+++ src/js/components/PlayerCard.vue
@@ -297,25 +297,25 @@ export default {
         this.cardAttributes.onload = this.render
         this.cardAttributes.src = '../res/img/card_attributes.png';
 
-        var f = new FontFace('MatrixRegularSmallCaps', 'url(../res/font/MatrixRegularSmallCaps.woff2)');
+        var f = new FontFace('MatrixRegularSmallCaps', 'url("../res/font/MatrixRegularSmallCaps.woff2") format("woff2")');
         f.load().then(font => {
             document.fonts.add(font)
             this.render()
         })
 
-        f = new FontFace('ITCStoneSerifSmallCapsBold', 'url(../res/font/ITCStoneSerifSmallCapsBold.woff2)');
+        f = new FontFace('ITCStoneSerifSmallCapsBold', 'url("../res/font/ITCStoneSerifSmallCapsBold.woff2") format("woff2")');
         f.load().then(font => {
             document.fonts.add(font)
             this.render()
         })
 
-        f = new FontFace('StoneSerifRegular', 'url(../res/font/StoneSerifRegular.woff2)');
+        f = new FontFace('StoneSerifRegular', 'url("../res/font/StoneSerifRegular.woff2") format("woff2")');
         f.load().then(font => {
             document.fonts.add(font)
             this.render()
         })
 
-        f = new FontFace('MatrixBoldSmallCaps', 'url(../res/font/MatrixBoldSmallCaps.woff2)');
+        f = new FontFace('MatrixBoldSmallCaps', 'url("../res/font/MatrixBoldSmallCaps.woff2") format("woff2")');
         f.load().then(font => {
             document.fonts.add(font)
             this.render()
