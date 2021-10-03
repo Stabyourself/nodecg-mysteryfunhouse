da661371dcd0ba0b8d497070e21c52024469e4ce
Maurice <maurice@guegan.de>
2021-10-03 18:13:01 +0200

    Adjust path design

--- src/js/components/waitscreen/PlayerPath.vue
+++ src/js/components/waitscreen/PlayerPath.vue
@@ -2,9 +2,38 @@
     <div class="player-path" v-if="info">
         <h1>{{ apostrophe(info.name) }} matches</h1>
 
-        <match-history-match v-for="match of info.matches" :key="match.id"
-            :match="match">
-        </match-history-match>
+        <v-timeline
+            dense
+            light
+            >
+            <v-timeline-item
+            v-for="match of info.matches"
+            :key="match.id"
+            large
+            class="align-center"
+            color="transparent"
+            >
+                <template v-slot:icon>
+                    <v-avatar>
+                        <img :src="match.players[1].avatar">
+                    </v-avatar>
+                </template>
+
+
+                <div class="match">
+                    <div class="round">{{ match.round }}</div>
+                    <div class="opponent">{{ match.players[1].name }}</div>
+
+                    <div class="score"
+                        :class="{
+                            'green--text': match.winner == 0,
+                            'red--text': match.winner == 1,
+                        }"
+                    >{{ match.score }}</div>
+                </div>
+
+            </v-timeline-item>
+        </v-timeline>
     </div>
 </template>
 
