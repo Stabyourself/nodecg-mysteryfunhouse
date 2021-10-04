674bd6014cf30d678b1644ccf1f7b4e66d7434a9
Maurice <maurice@guegan.de>
2021-10-04 08:57:38 +0200

    Keep path heading in view

--- src/js/components/waitscreen/PlayerPath.vue
+++ src/js/components/waitscreen/PlayerPath.vue
@@ -2,45 +2,60 @@
     <div class="player-path" v-if="info">
         <h1>{{ apostrophe(info.name) }} matches</h1>
 
-        <v-timeline
-            v-if="info.matches.length > 0"
-            dense
-            light
-            >
-            <v-timeline-item
-            v-for="match of info.matches"
-            :key="match.id"
-            large
-            class="align-center"
-            color="transparent"
-            >
-                <template v-slot:icon>
-                    <v-avatar>
-                        <img :src="match.players[1].avatar">
-                    </v-avatar>
-                </template>
+        <div class="timeline-wrap" ref="timelineWrap">
+            <v-timeline
+                v-if="info.matches.length > 0"
+                dense
+                light
+                >
+                <v-timeline-item
+                v-for="match of info.matches"
+                :key="match.id"
+                large
+                class="align-center"
+                color="transparent"
+                >
+                    <template v-slot:icon>
+                        <v-avatar>
+                            <img :src="match.players[1].avatar">
+                        </v-avatar>
+                    </template>
 
 
-                <div class="match">
-                    <div class="round">{{ match.round }}</div>
-                    <div class="opponent">{{ match.players[1].name }}</div>
+                    <div class="match">
+                        <div class="round">{{ match.round }}</div>
+                        <div class="opponent">{{ match.players[1].name }}</div>
 
-                    <div class="score"
-                        :class="{
-                            'green--text': match.winner == 0,
-                            'red--text': match.winner == 1,
-                        }"
-                    >{{ match.score }}</div>
-                </div>
+                        <div class="score"
+                            :class="{
+                                'green--text': match.winner == 0,
+                                'red--text': match.winner == 1,
+                            }"
+                        >{{ match.score }}</div>
+                    </div>
 
-            </v-timeline-item>
-        </v-timeline>
+                </v-timeline-item>
+            </v-timeline>
 
-        <div v-else style="font-size: 1.5em;">
-            No matches yet!
+            <div v-else style="font-size: 1.5em;">
+                No matches yet!
+            </div>
         </div>
     </div>
 </template>
+
+<style scoped lang="scss">
+    .timeline-wrap {
+        height: calc(100% - 40px);
+        overflow-y: auto;
+
+        -ms-overflow-style: none;  /* Internet Explorer 10+ */
+        scrollbar-width: none;  /* Firefox */
+        &::-webkit-scrollbar {
+            display: none;  /* Safari and Chrome */
+        }
+    }
+</style>
 
 <script>
 import { apostrophe } from "../../util.js"
@@ -56,7 +71,7 @@ export default {
             clearInterval(this.interval)
 
             setTimeout(() => {
-                let el = this.$el
+                let el = this.$refs.timelineWrap
 
                 if (el.scrollTo) {
                     var height = el.scrollHeight - el.offsetHeight;
