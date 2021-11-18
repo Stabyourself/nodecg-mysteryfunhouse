0380f842b0afc53751b4e4e8d97ff9b2bd3ae6b3
Maurice <maurice@guegan.de>
2021-11-18 04:26:23 +0100

    Extract vendor for smaller updates?

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,11 +3,12 @@ require('vuetifyjs-mix-extension')
 
 mix
     .js('src/js/main.js', "dist/js")
+    .extract()
     .sass('src/scss/style.scss', "dist/css")
 
     .sourceMaps(false)
     .vuetify('vuetify-loader', 'src/scss/vuetify-variables.scss')
-    .vue()
+    .vue(2)
     .options({
         processCssUrls: false
     })
