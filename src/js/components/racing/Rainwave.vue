25c2fa3c1c0ee955a7a11faf4d83426219f519b8
Maurice <maurice@guegan.de>
2021-12-04 19:07:21 +0100

    Fix rainwave showing wrong channel

--- src/js/components/racing/Rainwave.vue
+++ src/js/components/racing/Rainwave.vue
@@ -5,7 +5,7 @@
       height="94"
       frameBorder="0"
       scrolling="no"
-      src="https://rainwave.cc/widget/widget?sid=1&layout=art_right&text_align=right&anim_in=to_bottom&anim_out=to_right&art_size=70px&font_size=15pt&max_width=360px&np_header=no&ad=0&show_artist=true&delay=3"
+      src="https://rainwave.cc/widget/widget?sid=2&layout=art_right&text_align=right&anim_in=to_bottom&anim_out=to_right&art_size=70px&font_size=15pt&max_width=360px&np_header=no&ad=0&show_artist=true&delay=3"
     ></iframe>
   </div>
 </template>
