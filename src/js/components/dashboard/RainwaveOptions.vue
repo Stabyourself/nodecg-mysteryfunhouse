105b44a39e453817030e00ce0450183e6139c704
Maurice <maurice@guegan.de>
2021-12-12 10:51:25 +0100

    Add volume bar to rainwave

--- src/js/components/dashboard/RainwaveOptions.vue
+++ src/js/components/dashboard/RainwaveOptions.vue
@@ -8,6 +8,21 @@
           src="http://ocrstream.rainwave.cc:8000/ocremix.mp3"
         ></audio>
 
+        <v-slider v-model="volume" label="Volume" min="0" max="100">
+          <template v-slot:append>
+            <v-text-field
+              v-model="volume"
+              class="mt-0 pt-0"
+              type="number"
+              style="width: 45px"
+              min="0"
+              max="100"
+              step="1"
+              dense
+            ></v-text-field>
+          </template>
+        </v-slider>
+
         <v-switch v-model="showRainwave" label="Show Rainwave Info"></v-switch>
       </v-container>
     </v-main>
@@ -29,12 +44,19 @@ export default {
   },
 
   mounted() {
-    this.$refs["audio"].volume = 0.5;
+    this.$refs.audio.volume = this.volume / 100;
+  },
+
+  watch: {
+    volume(newVal) {
+      this.$refs.audio.volume = newVal / 100;
+    },
   },
 
   data() {
     return {
       showRainwave: false,
+      volume: 50,
     };
   },
 };
