029745bc5df511ee4b78bd1420aba9f503f660af
Maurice <maurice@guegan.de>
2022-03-19 19:24:22 +0100

    Add 2 player funfest layout

---
+++ src/js/components/funfest/FunfestTwoPlayer.vue
@@ -0,0 +1,344 @@
+<template>
+  <v-app>
+    <swipe
+      :visible="visible"
+      dir="down"
+      :delay="0.5"
+      style="top: 10px"
+      class="match-round"
+    >
+      <div class="logo">MYSTERY<br />FUN FEST</div>
+    </swipe>
+
+    <player-name
+      :visible="visible"
+      pronoun-h="40"
+      name-h="95"
+      style="top: 15px; left: 20px; width: 775px; height: 135px"
+      class="player-name"
+    >
+      <template v-slot:pronouns>
+        <swipe :visible="visible" dir="up" :delay="0.5">
+          <span class="pixel-font">{{ player0pronouns }}</span>
+        </swipe>
+      </template>
+      <template v-slot:name>
+        <swipe dir="up" :visible="visible">
+          <fit-text :max="2.5" class="pixel-font">
+            {{ player0name
+            }}<img
+              v-if="player0volume > 0"
+              :src="'/bundles/nodecg-mysteryfunhouse/dist/img/audio.png'"
+            />
+          </fit-text>
+        </swipe>
+      </template>
+    </player-name>
+
+    <player-name
+      :visible="visible"
+      class="right"
+      pronoun-h="40"
+      name-h="95"
+      style="top: 15px; right: 15px; width: 775px; height: 135px"
+    >
+      <template v-slot:pronouns>
+        <swipe :visible="visible" dir="up" :delay="0.5">
+          <span class="pixel-font">
+            {{ player1pronouns }}
+          </span>
+        </swipe>
+      </template>
+      <template v-slot:name>
+        <swipe dir="up" :visible="visible">
+          <fit-text :max="2.5" class="pixel-font">
+            <img
+              v-if="player1volume > 0"
+              :src="'/bundles/nodecg-mysteryfunhouse/dist/img/audio.png'"
+            />{{ player1name }}
+          </fit-text>
+        </swipe>
+      </template>
+    </player-name>
+
+    <div
+      style="
+        position: absolute;
+        top: 150px;
+        left: 15px;
+        width: 930px;
+        height: 698px;
+      "
+    >
+      <twitch-player
+        :opacity="player0streamHidden ? 0 : 1"
+        :playerNumber="0"
+        :url="player0twitch"
+        :quality="player0quality ? player0quality : 'auto'"
+        :volume="player0streamHidden || !visible ? 0 : player0volume"
+        :crop="player0crop"
+        :aspectratio="player0aspectratio"
+        :width="930"
+        :height="698"
+      ></twitch-player>
+    </div>
+
+    <div
+      style="
+        position: absolute;
+        top: 150px;
+        left: 975px;
+        width: 930px;
+        height: 698px;
+      "
+    >
+      <twitch-player
+        :opacity="player1streamHidden ? 0 : 1"
+        :playerNumber="1"
+        :url="player1twitch"
+        :quality="player1quality ? player1quality : 'auto'"
+        :volume="player1streamHidden || !visible ? 0 : player1volume"
+        :crop="player1crop"
+        :aspectratio="player1aspectratio"
+        :width="930"
+        :height="698"
+      ></twitch-player>
+    </div>
+
+    <player-done-slider
+      style="top: 848px; left: 15px; width: 930px"
+      :state="player0raceState"
+      :finalTime="player0finalTime"
+    >
+    </player-done-slider>
+
+    <player-done-slider
+      style="top: 848px; left: 975px; width: 930px"
+      :state="player1raceState"
+      :finalTime="player1finalTime"
+    >
+    </player-done-slider>
+
+    <game-box style="top: 865px; left: 15px">
+      <template v-slot:boxart>
+        <swipe
+          :visible="visible"
+          dir="right"
+          class="boxart"
+          v-if="currentBoxart"
+        >
+          <div class="d-flex align-center" style="height: 100%">
+            <img :src="currentBoxart.url" />
+          </div>
+        </swipe>
+      </template>
+
+      <template v-slot:text>
+        <swipe :visible="visible" dir="up" :delay="1" class="game">
+          <fit-text :max="1" :min="0.1">
+            <span class="pixel-font">
+              {{ game }}
+              <span v-if="!!platform"> ({{ platform }})</span>
+            </span>
+          </fit-text>
+        </swipe>
+        <swipe :visible="visible" dir="up" :delay="0.8" class="goal">
+          <fit-text :max="1" :min="0.1">
+            <span class="pixel-font">
+              {{ goal }}
+              <span v-if="!!submitter"> - Subbed by {{ submitter }}</span>
+            </span>
+          </fit-text>
+        </swipe>
+        <!-- <div class="submitter">Submitted by {{ submitter }}</div> -->
+      </template>
+    </game-box>
+
+    <!-- <rainwave
+            v-if="showRainwave"
+            style="top: 975px; left: 1521px; width: 444px; height: 124px">
+        </rainwave> -->
+
+    <timer style="top: 848px" :class="{ active: timer.state == 'playing' }">
+      <swipe dir="down" :visible="visible">
+        {{ timerText }}
+      </swipe>
+    </timer>
+  </v-app>
+</template>
+
+<style lang="scss" scoped>
+.player-name {
+  img {
+    margin-left: 0.3em;
+    margin-right: 0.3em;
+
+    height: 0.8em;
+    vertical-align: text-top;
+  }
+}
+
+.boxart {
+  margin-right: 15px;
+  height: 100%;
+
+  img {
+    display: block;
+    margin: 0 auto;
+    max-height: 100%;
+    max-width: 325px;
+  }
+}
+
+.logo {
+  font-size: 1.5em;
+  line-height: 1;
+
+  font-family: Rye, serif;
+  font-weight: 400;
+  padding: 0.2em 0.5em;
+  text-align: center;
+  text-shadow: 5px 5px 0 #eb452b, 10px 10px 0 #efa032, 15px 15px 0 #46b59b,
+    20px 20px 0 #017e7f, 25px 25px 0 #052939;
+}
+
+.round {
+  margin-top: -30px;
+}
+
+.match-round {
+  margin-top: -26px;
+}
+</style>
+
+<script>
+import { bindReplicant, formatTimer } from "../../util.js";
+
+export default {
+  created() {
+    bindReplicant.call(this, "game");
+    bindReplicant.call(this, "goal");
+    bindReplicant.call(this, "platform");
+    bindReplicant.call(this, "submitter");
+    bindReplicant.call(this, "currentBoxart");
+
+    bindReplicant.call(this, "timer");
+
+    bindReplicant.call(this, "showRainwave");
+
+    bindReplicant.call(this, "round", "match1round");
+
+    for (let i = 0; i < 2; i++) {
+      bindReplicant.call(this, `player${i}name`);
+      bindReplicant.call(this, `player${i}pronouns`);
+
+      bindReplicant.call(this, `player${i}twitch`);
+      // bindReplicant.call(this, `player${i}quality`)
+      bindReplicant.call(this, `player${i}volume`);
+      bindReplicant.call(this, `player${i}streamHidden`);
+      bindReplicant.call(this, `player${i}aspectratio`);
+
+      bindReplicant.call(this, `player${i}raceState`);
+      bindReplicant.call(this, `player${i}finalTime`);
+
+      bindReplicant.call(this, `player${i}crop`);
+    }
+
+    if (
+      window.obsstudio &&
+      window.obsstudio.getControlLevel &&
+      window.obsstudio.getControlLevel != 0
+    ) {
+      window.obsstudio.getCurrentScene((scene) => {
+        console.log("Start scene: " + scene.name);
+        if (scene.name == "2 Player") {
+          this.visible = true;
+        }
+      });
+
+      window.addEventListener("obsSceneChanged", (event) => {
+        console.log("Switched to scene " + event.detail.name);
+        if (event.detail.name == "Funfest 2 Player") {
+          this.visible = true;
+        } else {
+          this.visible = false;
+        }
+      });
+    } else {
+      this.visible = false;
+      setTimeout(() => {
+        this.visible = true;
+      }, 0);
+    }
+
+    document.addEventListener("keyup", (e) => {
+      if (e.key === "Enter") {
+        this.visible = !this.visible;
+      }
+    });
+
+    let vue = this;
+    nodecg.listenFor("playSound", vue.playSound);
+  },
+
+  methods: {
+    playSound(data) {
+      nodecg.playSound(data.sound);
+    },
+  },
+
+  computed: {
+    timerText() {
+      return formatTimer(this.timer.ms, false, false);
+    },
+  },
+
+  data() {
+    return {
+      game: "",
+      goal: "",
+      platform: "",
+      submitter: "",
+      currentBoxart: "",
+
+      round: "",
+
+      player0name: "",
+      player0pronouns: "",
+
+      player0twitch: "",
+      player0quality: null,
+      player0volume: 0,
+      player0streamHidden: false,
+
+      player0raceState: "none",
+      player0finalTime: "",
+
+      player0crop: [0, 0, 0, 0],
+      player0aspectratio: false,
+
+      player1name: "",
+      player1pronouns: "",
+
+      player1twitch: "",
+      player1quality: null,
+      player1volume: 0,
+      player1streamHidden: false,
+
+      player1raceState: "none",
+      player1finalTime: "",
+
+      player1crop: [0, 0, 0, 0],
+      player1aspectratio: false,
+
+      timer: {
+        ms: 0,
+      },
+
+      showRainwave: false,
+
+      visible: false,
+    };
+  },
+};
+</script>
