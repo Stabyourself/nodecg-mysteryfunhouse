bff5bfeec2e9bebdbcd2434797fb9f800ab0ebb9
Maurice <maurice@guegan.de>
2023-12-18 16:41:48 +0100

    Update image uploader

--- extension/boxart-uploader.js
+++ extension/boxart-uploader.js
@@ -1,52 +1,58 @@
-const ctx = require("./nodecg");
+const ctx = require('./nodecg');
 const nodecg = ctx.get();
 
-function uploadBoxart(url) {
-  let whitelist = ["jpg", "jpeg", "png", "gif", "webp"];
+const boxartsRep = nodecg.Replicant('assets:boxarts', { defaultValue: [] });
+const currentBoxartRep = nodecg.Replicant('currentBoxart', { defaultValue: '' });
 
-  let ext = url.split(".").pop();
+function uploadBoxart(url) {
+  let whitelist = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
+
+  let ext = url.split('.').pop();
   if (!whitelist.includes(ext)) {
-    nodecg.log.error("Invalid file type");
+    nodecg.log.error('Invalid file type');
     return;
   }
 
-  let filename = url.split("/").pop();
+  let filename = url.split('/').pop();
   let timestamp = Date.now();
   let filepath = `assets/nodecg-mysteryfunhouse/boxarts/${timestamp}-${filename}`;
 
   download(url, filepath, () => {});
 }
 
-const request = require("request");
-var fs = require("fs");
+const request = require('request');
+var fs = require('fs');
 
 const download = (url, dest, cb) => {
   const file = fs.createWriteStream(dest);
   const sendReq = request.get(url);
 
   // verify response code
-  sendReq.on("response", (response) => {
+  sendReq.on('response', (response) => {
     console.log(response.statusCode);
     if (response.statusCode !== 200) {
       file.close(cb);
       fs.unlink(dest, () => cb());
-      return cb("Response status was " + response.statusCode);
+      return cb('Response status was ' + response.statusCode);
     }
 
     sendReq.pipe(file);
   });
 
   // close() is async, call cb after close completes
-  file.on("finish", () => file.close(cb));
+  file.on('finish', () => {
+    currentBoxartRep.value = { ...boxartsRep.value[boxartsRep.value.length - 1] };
+    file.close(cb);
+  });
 
   // check for request errors
-  sendReq.on("error", (err) => {
+  sendReq.on('error', (err) => {
     fs.unlink(dest, () => cb(err.message)); // delete the (partial) file and then return the error
   });
 
-  file.on("error", (err) => {
+  file.on('error', (err) => {
     // Handle errors
     fs.unlink(dest, () => cb(err.message)); // delete the (partial) file and then return the error
   });
 };
-nodecg.listenFor("uploadBoxart", uploadBoxart);
+nodecg.listenFor('uploadBoxart', uploadBoxart);
