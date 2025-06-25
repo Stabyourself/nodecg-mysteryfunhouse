b545bf592dfb65e41276e2512ec5cda3880a223f
Maurice <maurice@guegan.de>
2025-06-25 06:10:01 +0200

    Fix upload so it's only on gameoptions

--- src/js/components/dashboard/GameOptions.vue
+++ src/js/components/dashboard/GameOptions.vue
@@ -4,16 +4,9 @@
       <v-container>
         <v-text-field v-model="game" label="Game name"></v-text-field>
 
-        <label
-          class="v-label v-label--active theme--dark"
-          style="font-size: 12px"
-          >Boxart</label
-        >
+        <label class="v-label v-label--active theme--dark" style="font-size: 12px">Boxart</label>
         <div class="select-img-wrap mb-3" nodecg-dialog="boxart-select-dialog">
-          <img
-            class="select-img"
-            :src="currentBoxart ? currentBoxart.url : ''"
-          />
+          <img class="select-img" :src="currentBoxart ? currentBoxart.url : ''" />
           <div class="select-img-border"></div>
         </div>
 
@@ -28,42 +21,42 @@
 </template>
 
 <script>
-import { bindReplicant } from "../../util.js";
-
-document.onpaste = function (event) {
-    var items = (event.clipboardData || event.originalEvent.clipboardData).items;
-    console.log(JSON.stringify(items)); // might give you mime types
-    for (var index in items) {
-        var item = items[index];
-        if (item.kind === 'file') {
-            var blob = item.getAsFile();
-            var reader = new FileReader();
-            reader.onload = function (event) {
-                console.log("arrayBuffer: ", event.target.result); // data url!
-                nodecg.sendMessage("uploadBoxart", event.target.result);
-            };
-            reader.readAsDataURL(blob);
-        }
-    }
-};
+import { bindReplicant } from '../../util.js';
 
 export default {
   created() {
-    bindReplicant.call(this, "game");
-    bindReplicant.call(this, "goal");
-    bindReplicant.call(this, "platform");
-    bindReplicant.call(this, "submitter");
-    bindReplicant.call(this, "currentBoxart");
+    bindReplicant.call(this, 'game');
+    bindReplicant.call(this, 'goal');
+    bindReplicant.call(this, 'platform');
+    bindReplicant.call(this, 'submitter');
+    bindReplicant.call(this, 'currentBoxart');
+
+    document.onpaste = function (event) {
+      var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+      console.log(JSON.stringify(items)); // might give you mime types
+      for (var index in items) {
+        var item = items[index];
+        if (item.kind === 'file') {
+          var blob = item.getAsFile();
+          var reader = new FileReader();
+          reader.onload = function (event) {
+            console.log('arrayBuffer: ', event.target.result); // data url!
+            nodecg.sendMessage('uploadBoxart', event.target.result);
+          };
+          reader.readAsDataURL(blob);
+        }
+      }
+    };
   },
 
   data() {
     return {
-      game: "",
-      goal: "",
-      platform: "",
-      submitter: "",
+      game: '',
+      goal: '',
+      platform: '',
+      submitter: '',
       currentBoxart: {},
-      boxartUrl: "",
+      boxartUrl: '',
     };
   },
 };
